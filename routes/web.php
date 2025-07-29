fe2f64a2c740840097a0649e2c4dad2019ba3443
Md Raza <mdraza8397@gmail.com>
2025-07-29 14:42:02 +0530

    second commit

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::get('/teas', function () {
 Route::get('/teas/{id}', function ($id) {
     $teas = [
         ["name" => "Masala Chai", "price" => 199, "id" => 1],
-        ["name" => "Ginger Chai", "price" => 199, "id" => 2],
-        ["name" => "Assam Chai", "price" => 199, "id" => 3],
+        ["name" => "Ginger Chai", "price" => 299, "id" => 2],
+        ["name" => "Assam Chai", "price" => 99, "id" => 3],
     ];
     return view('teas.teadetail', ['tea' => $teas[$id-1]]);
 });
